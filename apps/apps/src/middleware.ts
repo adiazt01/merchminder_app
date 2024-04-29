598f0d576d54e324bfcb1f3136945ea11e90704f
adiazt01 <adiaztovar2004@gmail.com>
2024-04-29 13:14:20 -0400

    fix(app): refactor to use new login route

--- apps/apps/src/middleware.ts
+++ apps/apps/src/middleware.ts
@@ -1,12 +1,15 @@
 import { auth } from "@/utils/auth";
 import { NextRequest, NextResponse } from "next/server";
 
+// @ts-ignore
 export default auth((req: NextRequest) => {
+  // @ts-ignore
   if (!req.auth) {
     return NextResponse.redirect(
-      new URL("/api/auth/signin", req.nextUrl).toString()
+      new URL("/auth/login", req.nextUrl).toString()
     );
   }
+  return NextResponse.next();
 });
 
 export const config = {
